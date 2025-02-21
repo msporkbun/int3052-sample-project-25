57fc7430262576f2803db75307f22a7a794cbe5e
Patty <pattamon.nakvi@gmail.com>
2025-02-21 11:55:27 +1100

    Updated the style on the signup.tsx, including container, form and text

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,17 @@
 // name of the file can be anything
 import { View, Text, StyleSheet } from "react-native";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Login() {
   return (
-    <View>
-      <Text>Login</Text>
-      <Link href="/signup">
-        <Text>Signup</Text>
-      </Link>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>Login</Text>
+        <Link href="/signup">
+          <Text>Go to Signup</Text>
+        </Link>
+      </View>
+    </SafeAreaView>
   );
 }
--- app/signup.tsx
+++ app/signup.tsx
@@ -1,13 +1,31 @@
 import { View, Text, StyleSheet } from "react-native";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Signup() {
   return (
-    <View>
-      <Text>Signup</Text>
-      <Link href="/">
-        <Text>back</Text>
-      </Link>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.form}>
+        <Text style={styles.text}>Signup</Text>
+        <Link href="/">
+          <Text>Go to Login</Text>
+        </Link>
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#DEF9F4",
+  },
+  form: {
+    marginTop: 50,
+  },
+  text: {
+    fontSize: 32,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
